3d18b5adaabf1b8aa7a44ce0eecc8132a7fb0df0
dimaslearningbusiness-web <dimaslearningbusiness@gmail.com>
2026-02-04 19:31:13 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 import path from "path"
 
 export default defineConfig({
-  // Esta linha diz ao site para funcionar no subdiretório do GitHub
   base: '/dimas-lingua-tech/',
   plugins: [react()],
   resolve: {
