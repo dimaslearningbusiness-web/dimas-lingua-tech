1a66bd09c4b4cb865c44badb6eba04e5758ed11f
dimaslearningbusiness-web <dimaslearningbusiness@gmail.com>
2026-02-04 19:30:58 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import AdminDashboard from "./pages/AdminDashboard";
-import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from "./components/ui/toaster";
 import './index.css';
 
 function App() {
