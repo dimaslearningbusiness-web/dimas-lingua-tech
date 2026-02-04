c8e5a1cb5a9120b16d735c8cabfee3496f607b0a
dimaslearningbusiness-web <dimaslearningbusiness@gmail.com>
2026-02-04 18:47:14 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from "path"
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+export default defineConfig({
+  // ADICIONA ESTA LINHA ABAIXO (usa o nome exato do teu repositório no GitHub)
+  base: '/dimas-lingua-tech/', 
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+})
