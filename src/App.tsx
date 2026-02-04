116d447bb8514464998675839beccf4327be7400
dimaslearningbusiness-web <dimaslearningbusiness@gmail.com>
2026-02-04 19:21:09 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import AdminDashboard from "./pages/AdminDashboard";
 import { Toaster } from "@/components/ui/toaster";
-import './index.css'
+import './index.css';
+
 function App() {
   return (
     <>
-
-
-// No teu src/App.tsx
-<BrowserRouter basename="/dimas-lingua-tech">
-  <Routes>
-    <Route path="/" element={<Index />} />
-    <Route path="/admin" element={<AdminDashboard />} />
-  </Routes>
-</BrowserRouter>
-
-
-
-
-
-     
+      <BrowserRouter basename="/dimas-lingua-tech">
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/admin" element={<AdminDashboard />} />
+        </Routes>
+      </BrowserRouter>
+      <Toaster />
     </>
   );
 }
