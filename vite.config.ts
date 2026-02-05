a57688b0aee440397234097221343f20782c36d8
dimaslearningbusiness-web <dimaslearningbusiness@gmail.com>
2026-02-05 11:01:13 +0100

    Atualizar o vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import path from "path"
 
 export default defineConfig({
-  base: '/dimas-lingua-tech/',
+  base: './', // Mudamos de '/dimas-lingua-tech/' para './'
   plugins: [react()],
   resolve: {
     alias: {
