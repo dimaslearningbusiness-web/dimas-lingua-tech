15b9ab7ae1cb4609b8b110396cc737071a0f6b90
dimaslearningbusiness-web <dimaslearningbusiness@gmail.com>
2026-02-05 11:01:38 +0100

    Atualizar o App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import AdminDashboard from "./pages/AdminDashboard";
 import { Toaster } from "@/components/ui/toaster";
-import './index.css';
 
 function App() {
   return (
-    <BrowserRouter basename="/dimas-lingua-tech">
+    <HashRouter> 
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="/admin" element={<AdminDashboard />} />
       </Routes>
       <Toaster />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
 export default App;
-
