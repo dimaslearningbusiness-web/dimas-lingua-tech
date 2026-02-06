750eed20e9f487d4bbc34c015f9088f5cbd07ec9
dimaslearningbusiness-web <dimaslearningbusiness@gmail.com>
2026-02-06 22:16:39 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc' // Mudamos de 'react' para 'react-swc'
 
 export default defineConfig({
   plugins: [react()],
   base: '/dimas-lingua-tech/',
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
 })
